5257e5a835fc3624b5a7a738fd2a15ac37c79a31
bobsledding <bobsledding@hotmail.com>
2020-07-10 08:25:09 +0800

    從購物車刪除done

--- portfolio_cart/cart/urls.py
+++ portfolio_cart/cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add/', views.add_to_cart, name='add')
+    path('add/', views.add_to_cart, name='add'),
+    path('remove/', views.remove_from_cart, name='remove'),
 ]
