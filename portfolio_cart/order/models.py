d413149474944cc01e6448ea35e38099ff8f6a21
bobsledding <bobsledding@hotmail.com>
2020-07-19 00:18:48 +0800

    可以補付款了，取消訂單也做好了，接著要把order與cart的一些view改成require POST

--- portfolio_cart/order/models.py
+++ portfolio_cart/order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.core import serializers
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 """
 在order必須保存的資訊
@@ -12,6 +13,9 @@ from django.core import serializers
 """
 
 class Order(models.Model):
+    class Meta:
+        ordering = ['-datetime_create']
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime_create = models.DateTimeField('建立日期',auto_now_add=True)
     datetime_edit = models.DateTimeField('修改日期',auto_now=True)
@@ -45,8 +49,22 @@ class Order(models.Model):
 
 class Payment(models.Model):
 
+    class PaymentTypes(models.TextChoices):
+        CREDIT = 'Credit', _('信用卡及 GooglePay')
+        GOOGLEPAY = 'GooglePay', _('GooglePay') # (若為PC版時不支援)
+        WEBATM = 'WebATM', _('網路 ATM') # (若為手機版時不支援)
+        ATM = 'ATM', _('自動櫃員機')
+        CVS = 'CVS', _('超商代碼')
+        BARCODE = 'BARCODE', _('超商條碼') # (若為手機版時不支援)
+        ALL = 'ALL', _('不指定付款方式，由綠界顯示付款方式選擇頁面。')
+
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     trade_no = models.CharField('廠商交易編號',max_length=20)
+    type = models.CharField(
+        max_length=9,
+        choices=PaymentTypes.choices,
+        default=PaymentTypes.CREDIT,
+    )
     is_success = models.BooleanField('交易是否成功',default=False)
     datetime_create = models.DateTimeField('建立日期',auto_now_add=True)
     datetime_edit = models.DateTimeField('修改日期',auto_now=True)
