d2ececc8750d7241df40218d37fe5cad7c860882
bobsledding <bobsledding@hotmail.com>
2020-07-19 11:56:49 +0800

    給car與order加上post_required與login_required，接著要把form斟酌重修。

--- portfolio_cart/order/urls.py
+++ portfolio_cart/order/urls.py
@@ -6,8 +6,8 @@ app_name = 'order'
 urlpatterns = [
     path('', views.index, name='index'),
     path('ecpay/', views.ecpay_view, name='ecpay_view'),
-    path('ecpay/<int:order_id>/', views.pay_from_index, name='pay_from_index'),
+    path('pay_from_index/', views.pay_from_index, name='pay_from_index'),
     path('result/', views.result, name='result'),
     path('receive_from_ecpay/', views.receive_from_ecpay, name='receive_from_ecpay'),
-    path('cancel/<int:order_id>/', views.cancel, name='cancel'),
+    path('cancel/', views.cancel, name='cancel'),
 ]
