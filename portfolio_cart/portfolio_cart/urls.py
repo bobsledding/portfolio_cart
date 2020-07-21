b81ecb389cc11f82e7928e66c434fb2ea881068c
bobsledding <bobsledding@hotmail.com>
2020-07-22 01:47:01 +0800

    base.html大概改好了，要開始找生活照放進去，並且寫好about.html。

--- portfolio_cart/portfolio_cart/urls.py
+++ portfolio_cart/portfolio_cart/urls.py
@@ -32,6 +32,7 @@ accounts/reset/done/ [name='password_reset_complete']
 """
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('products/', include('products.urls')),
--- portfolio_cart/portfolio_cart/views.py
+++ portfolio_cart/portfolio_cart/views.py
@@ -11,3 +11,6 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
