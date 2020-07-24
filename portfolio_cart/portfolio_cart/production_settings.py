dc6db0e6670d4980d18d2414e4e824702712a556
bobsledding <bobsledding@hotmail.com>
2020-07-24 19:58:43 +0800

    set_up_for_s3

--- portfolio_cart/portfolio_cart/production_settings.py
+++ portfolio_cart/portfolio_cart/production_settings.py
@@ -17,3 +17,26 @@ ALLOWED_HOSTS = ['.bobsledding-cart.herokuapp.com']
 
 # Turn off DEBUG mode.
 DEBUG = False
+
+# Config S3
+USE_S3 = True
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = "AKIAXRK2KMSGCFG7YH3O"
+    AWS_SECRET_ACCESS_KEY = "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"
+    AWS_STORAGE_BUCKET_NAME = "bobsledding-cart"
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    # AWS_LOCATION = 'static'
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'products.storage_backends.PublicMediaStorage'
+# else:
+#     STATIC_URL = '/staticfiles/'
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
