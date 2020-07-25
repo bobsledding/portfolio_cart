19235b9f0234fda855c95e453c7759189ee27704
bobsledding <bobsledding@hotmail.com>
2020-07-25 15:10:30 +0800

    刪掉keys，改在heroku config裡設定。

--- portfolio_cart/portfolio_cart/production_settings.py
+++ portfolio_cart/portfolio_cart/production_settings.py
@@ -1,6 +1,7 @@
 # Import all default settings.
 from .settings import *
 import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(),
@@ -23,9 +24,9 @@ USE_S3 = True
 
 if USE_S3:
     # aws settings
-    AWS_ACCESS_KEY_ID = "AKIAXRK2KMSGCFG7YH3O"
-    AWS_SECRET_ACCESS_KEY = "XXXX"
-    AWS_STORAGE_BUCKET_NAME = "bobsledding-cart"
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
